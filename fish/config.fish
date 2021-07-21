453cbd7c51590bb1cdf7c142f229f3f50e45eddd
Ethan Brierley <ethanboxx@gmail.com>
2021-07-21 15:55:28 +0100

    fish

---
+++ fish/config.fish
@@ -0,0 +1,14 @@
+starship init fish | source
+
+alias ls='exa'
+alias ll='exa -lah'
+
+alias v='nvim'
+
+alias bin='gio trash'
+
+set -x CARGO_TARGET_DIR /home/ethan/cargo_target
+set -x FZF_DEFAULT_COMMAND rg
+set -x LIBVIRT_DEFAULT_URI "qemu:///system"
+set -x EDITOR nvim
+
