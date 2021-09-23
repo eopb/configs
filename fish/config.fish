d8aee4353bb3fcaaad1adb4cefcd5b7823d28b4b
Ethan Brierley <ethanboxx@gmail.com>
2021-09-23 09:33:49 +0100

    Fix cargo paths for MacOS

--- fish/config.fish
+++ fish/config.fish
@@ -8,9 +8,10 @@ alias v='nvim'
 alias bin='gio trash'
 alias rust-analyzer="rustup run nightly rust-analyzer"
 
-set -x CARGO_TARGET_DIR /home/ethan/cargo_target
+set -x CARGO_TARGET_DIR /Users/ethan.brierley/cargo_target
 set -x FZF_DEFAULT_COMMAND rg
 set -x LIBVIRT_DEFAULT_URI "qemu:///system"
 set -x EDITOR nvim
+set PATH $HOME/.cargo/bin $PATH
 
 fish_vi_key_bindings
